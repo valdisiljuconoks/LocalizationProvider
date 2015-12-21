70040b6dbb195ff8a0d1cade3ccef9509a99b553
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-12-22 01:50:10 +0200

    Converted tool to console application

---
+++ LocalizationProvider.MigrationTool/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using NDesk.Options;
+
+namespace TechFellow.LocalizationProvider.MigrationTool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var showHelp = false;
+            string sourceDirectory = null;
+            string targetDirectory = null;
+
+            var p = new OptionSet
+            {
+                {
+                    "s|sourceDir=", "web application source directory",
+                    v => sourceDirectory = v
+                },
+                {
+                    "t|targetDir=", "target directory where to write import script",
+                    v => targetDirectory = v
+                },
+                {
+                    "h|help", "show this message and exit",
+                    v => showHelp = v != null
+                }
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("greet: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `greet --help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(p);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(sourceDirectory))
+            {
+                Console.WriteLine("ERROR: Source directory parameter is missing!");
+                Console.WriteLine();
+                ShowHelp(p);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(targetDirectory))
+            {
+                targetDirectory = sourceDirectory;
+            }
+
+            Console.WriteLine($"Source dir - {sourceDirectory}");
+            Console.WriteLine($"Target dir - {targetDirectory}");
+        }
+
+        private static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: LocalizationProvider.MigrationTool.exe [OPTIONS]+");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
