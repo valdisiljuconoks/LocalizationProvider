560033d7bab6fb628c5a59cc6f49f69199b74244
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2016-02-14 21:13:04 +0200

    Updated assembly versions

---
+++ build-packages.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\valdis\Documents\GitHubVisualStudio\DbLocalizationProvider\.nuget
+
+.\nuget.exe pack ..\DbLocalizationProvider\DbLocalizationProvider.csproj -Properties Configuration=Release
+.\nuget.exe pack ..\DbLocalizationProvider.AdminUI\DbLocalizationProvider.AdminUI.csproj -Properties Configuration=Release
+.\nuget.exe pack ..\DbLocalizationProvider.MigrationTool\DbLocalizationProvider.MigrationTool.csproj -Properties Configuration=Release -tool
