520d0ec1bfa0db505cdad76a85ff5f5c1d1b7300
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2016-05-27 21:55:25 +0300

    Extracted even more EPiServer dependencies from Admin UI. WIP. #27

---
+++ DbLocalizationProvider/AvailableLanguagesProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace DbLocalizationProvider
+{
+    internal class AvailableLanguagesProvider : IAvailableLanguagesProvider
+    {
+        public IEnumerable<CultureInfo> GetAll()
+        {
+            return new List<CultureInfo>
+                   {
+                       new CultureInfo("en")
+                   };
+        }
+    }
+}
---
+++ DbLocalizationProvider/IAvailableLanguagesProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace DbLocalizationProvider
+{
+    public interface IAvailableLanguagesProvider
+    {
+        IEnumerable<CultureInfo> GetAll();
+    }
+}
