9997ea8c3dcb435dbbdabd2c6b4de1aecce9e89c
valdisiljuconoks <valdis@VALDIS-PC>
2016-09-05 17:14:06 +0300

    Added sample EPiServer site

---
+++ Tests/DbLocalizationProvider.EPiServer.Sample/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using EPiServer;
+
+namespace DbLocalizationProvider.EPiServer.Sample
+{
+    public class EPiServerApplication : Global
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
+        }
+    }
+}
