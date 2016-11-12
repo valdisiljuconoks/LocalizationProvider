bccb5e574ffed42aa74564bc3c52b0397d9880f7
valdisiljuconoks <valdis@VALDIS-PC>
2016-11-12 16:40:52 +0200

    Testing categories

---
+++ Tests/DbLocalizationProvider.EPiServer.Sample/Resources/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace DbLocalizationProvider.EPiServer.Sample.Resources
+{
+    [LocalizedResource(KeyPrefix = "/categories/")]
+    public class Categories
+    {
+        [ResourceKey("category[@name=\"" + nameof(SampleCategory) + "\"]/description")]
+        public static string SampleCategory => "This is sample cat. from code";
+    }
+}
