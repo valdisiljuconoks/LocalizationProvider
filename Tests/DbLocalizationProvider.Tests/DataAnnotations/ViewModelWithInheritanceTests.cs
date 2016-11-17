c9ada57a0c703e71d624773594d0a4b70853ba58
valdisiljuconoks <valdis@VALDIS-PC>
2016-11-17 04:22:51 +0200

    Some small tests

--- Tests/DbLocalizationProvider.Tests/DataAnnotations/ViewModelWithInheritanceTests.cs
+++ Tests/DbLocalizationProvider.Tests/DataAnnotations/ViewModelWithInheritanceTests.cs
@@ -10,10 +10,13 @@ namespace DbLocalizationProvider.Tests.DataAnnotations
         public void NotInheritedModel_ContainsOnlyDeclaredProperties()
         {
             var sut = new TypeDiscoveryHelper();
-            var properties = sut.ScanResources(typeof(SampleViewModelWithBase));
+            var properties = sut.ScanResources(typeof(SampleViewModelWithBase)).ToList();
             var keys = properties.Select(p => p.Key).ToList();
+            var stringLengthResource = properties.FirstOrDefault(r => r.Key == "DbLocalizationProvider.Tests.DataAnnotations.SampleViewModelWithBase.ChildProperty-StringLength");
 
             Assert.Contains("DbLocalizationProvider.Tests.DataAnnotations.SampleViewModelWithBase.ChildProperty-Description", keys);
+            Assert.NotNull(stringLengthResource);
+            Assert.Contains("StringLength", stringLengthResource.Translation);
             Assert.DoesNotContain("DbLocalizationProvider.Tests.DataAnnotations.SampleViewModelWithBase.BaseProperty", keys);
             Assert.DoesNotContain("DbLocalizationProvider.Tests.DataAnnotations.SampleViewModelWithBase.BaseProperty-Required", keys);
             Assert.DoesNotContain("DbLocalizationProvider.Tests.DataAnnotations.SampleViewModelWithBase.ChildProperty-Description-Required", keys);
