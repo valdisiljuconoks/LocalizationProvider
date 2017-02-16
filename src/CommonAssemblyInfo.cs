8144f5091ad3b1b913eecfbbe16e8a001f260369
valdisiljuconoks <valdis@VALDIS-PC>
2017-02-17 00:31:59 +0200

    Version bump

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Valdis Iljuconoks, http://blog.tech-fellow.net")]
 [assembly: AssemblyProduct("DbLocalizationProvider")]
-[assembly: AssemblyCopyright("Tech Fellow Consulting, Copyright © 2016")]
+[assembly: AssemblyCopyright("Tech Fellow Consulting, Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- src/DbLocalizationProvider.MigrationTool/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.MigrationTool/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DbLocalizationProvider.MigrationTool")]
 [assembly: AssemblyDescription("Migration tool to move Xml resources to database")]
 [assembly: Guid("5227c16b-342c-4097-a9ed-295cdd164a61")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
-[assembly: AssemblyInformationalVersion("3.1.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0")]
 
 [assembly: InternalsVisibleTo("DbLocalizationProvider.MigrationTool.Tests")]
