8fb8654cd47258f824b9095df76fa0790cd8fe1c
valdisiljuconoks <valdis@VALDIS-PC>
2017-02-24 13:21:57 +0200

    House cleaning

--- Tests/DbLocalizationProvider.EPiServer.Sample/Resources/Class1.cs
+++ Tests/DbLocalizationProvider.EPiServer.Sample/Resources/Class1.cs
@@ -4,5 +4,7 @@
     public class Class1
     {
         public static string MessageWithFormatArguments => "Here will be value `{0}`";
+
+        public static string TermsAndConditions => @"I have read the <a href=""{0}"">terms and conditions</a> and agree to them";
     }
 }
