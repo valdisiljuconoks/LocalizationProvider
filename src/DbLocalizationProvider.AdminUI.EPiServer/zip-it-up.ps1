d25c3e35e78d21b08aeadf769e8525e0276b0b49
valdisiljuconoks <valdis@VALDIS-PC>
2017-03-12 00:54:03 +0200

    Added script for zipping

---
+++ src/DbLocalizationProvider.AdminUI.EPiServer/zip-it-up.ps1
@@ -0,0 +1,9 @@
+﻿Add-Type -assembly "system.io.compression.filesystem"
+
+$moduleName = "DbLocalizationProvider.AdminUI.EPiServer"
+$source = $PSScriptRoot + "\modules\_protected\" + $moduleName
+$destination = $PSScriptRoot + "\" + $moduleName + ".zip"
+
+If(Test-path $destination) {Remove-item $destination}
+
+[io.compression.zipfile]::CreateFromDirectory($Source, $destination)
