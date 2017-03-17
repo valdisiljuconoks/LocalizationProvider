4ef3c7b9208a587defd9f97af8b1bc91b18cef2d
valdisiljuconoks <valdis@VALDIS-PC>
2017-03-17 17:54:10 +0200

    Added new translation helper method

--- src/DbLocalizationProvider/TranslationsExtensions.cs
+++ src/DbLocalizationProvider/TranslationsExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -11,10 +12,21 @@ namespace DbLocalizationProvider
             return ByLanguage(translations, language.Name);
         }
 
+        public static bool ExistsLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
+        {
+            if(string.IsNullOrEmpty(language))
+                throw new ArgumentNullException(nameof(language));
+
+            return translations?.FirstOrDefault(t => t.Language == language) != null;
+        }
+
         public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
         {
             if(translations == null)
                 return string.Empty;
+
+            if(string.IsNullOrEmpty(language))
+                throw new ArgumentNullException(nameof(language));
 
             var translation = translations.FirstOrDefault(t => t.Language == language);
             return translation != null ? translation.Value : string.Empty;
