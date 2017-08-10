ef82a2854be1565c9ed7d7ccb83f439b7a06cfae
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-08-10 23:11:26 +0300

    Fixed proper description for Xliff packages

--- src/DbLocalizationProvider.AdminUI.EPiServer.Xliff/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.AdminUI.EPiServer.Xliff/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbLocalizationProvider.AdminUI.EPiServer.Xliff")]
-[assembly: AssemblyDescription("XLIFF v2.0 export UI component for AdminUI")]
+[assembly: AssemblyDescription("XLIFF v2.0 export/import UI component for AdminUI")]
 [assembly: Guid("5435a71a-20f3-429f-999e-ab7aa409649e")]
 [assembly: AssemblyVersion("3.5.0.0")]
 [assembly: AssemblyFileVersion("3.5.0.0")]
--- src/DbLocalizationProvider.Xliff/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.Xliff/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbLocalizationProvider.Xliff")]
-[assembly: AssemblyDescription("Xliff v2.0 export library")]
+[assembly: AssemblyDescription("Xliff v2.0 export/import library")]
 [assembly: Guid("1b38b864-ffa5-498b-93bc-39f17b604202")]
 [assembly: AssemblyVersion("2.8.0.0")]
 [assembly: AssemblyFileVersion("2.8.0.0")]
