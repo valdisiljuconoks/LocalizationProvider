106c68131970d1fa7420522b7dafb949a149772f
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-08-10 22:04:38 +0300

    Version lift-up

--- src/DbLocalizationProvider.AdminUI.EPiServer.Xliff/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.AdminUI.EPiServer.Xliff/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbLocalizationProvider.AdminUI.EPiServer.Xliff")]
 [assembly: AssemblyDescription("XLIFF export UI component for AdminUI")]
 [assembly: Guid("5435a71a-20f3-429f-999e-ab7aa409649e")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyInformationalVersion("3.5.0")]
--- src/DbLocalizationProvider.Xliff/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.Xliff/Properties/AssemblyInfo.cs
@@ -5,8 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DbLocalizationProvider.Xliff")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("1b38b864-ffa5-498b-93bc-39f17b604202")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-
+[assembly: AssemblyVersion("2.8.0.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
+[assembly: AssemblyInformationalVersion("2.8.0")]
 [assembly:InternalsVisibleTo("DbLocalizationProvider.Xliff.Tests")]
