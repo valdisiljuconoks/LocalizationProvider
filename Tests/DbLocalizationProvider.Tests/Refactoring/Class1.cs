d1d6c006bcf16dafc1262ab7f8e4828e2112de54
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-09-07 23:18:19 +0300

    Started to work on migrations feature

---
+++ Tests/DbLocalizationProvider.Tests/Refactoring/Class1.cs
@@ -0,0 +1,25 @@
+﻿using DbLocalizationProvider.Abstractions.Refactoring;
+using Xunit;
+
+namespace DbLocalizationProvider.Tests.Refactoring
+{
+    [LocalizedResource]
+    [RenamedResource("OldResourceClass",
+                     OldNamespace = "In.Galaxy.Far.Far.Away")]
+    public class NewResourceClass
+    {
+        [RenamedResource("OldResourceKey")]
+        public static string NewResourceKey => "New Resource Key";
+    }
+}
+
+
+
+
+
+public class Class1
+    {
+        [Fact]
+        public void Test() { }
+    }
+}
---
+++ src/DbLocalizationProvider.Abstractions/Refactoring/RenamedResourceAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DbLocalizationProvider.Abstractions.Refactoring
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field)]
+    public class RenamedResourceAttribute : Attribute
+    {
+        public RenamedResourceAttribute(string oldName)
+        {
+            if(string.IsNullOrEmpty(oldName))
+                throw new ArgumentNullException(nameof(oldName));
+            
+            OldName = oldName;
+        }
+        
+        public string OldName { get; set; }
+        
+        public string OldNamespace { get; set; }
+    }
+}
