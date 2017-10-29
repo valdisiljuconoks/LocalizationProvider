4bed0a81b011dcf771d4f10292d9a746fe7c8cf0
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-10-29 21:57:14 +0200

    Converted Abstractions project to .Net Standard 2.0

--- Tests/DbLocalizationProvider.Core.AspNetSample/Startup.cs
+++ Tests/DbLocalizationProvider.Core.AspNetSample/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,7 +23,7 @@ namespace DbLocalizationProvider.Core.AspNetSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
@@ -40,11 +36,11 @@ namespace DbLocalizationProvider.Core.AspNetSample
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                       {
+                           routes.MapRoute(
+                                           name: "default",
+                                           template: "{controller=Home}/{action=Index}/{id?}");
+                       });
         }
     }
 }
