94ca0ad13d320d89d9aa6242bf0365331e7fb851
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-10-29 21:57:14 +0200

    Converted Abstractions project to .Net Standard 2.0

--- Tests/DbLocalizationProvider.Core.AspNetSample/Program.cs
+++ Tests/DbLocalizationProvider.Core.AspNetSample/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace DbLocalizationProvider.Core.AspNetSample
 {
@@ -19,7 +12,7 @@ namespace DbLocalizationProvider.Core.AspNetSample
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                   .UseStartup<Startup>()
+                   .Build();
     }
 }
