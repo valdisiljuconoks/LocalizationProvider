ab4e5a29f45b35194ee2dbbc167f059b9daa7742
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-12-03 01:27:13 +0200

    fixed core cache manager to get it compuiled

--- src/DbLocalizationProvider.AspNetCore/Cache/ClearCacheHandler.cs
+++ src/DbLocalizationProvider.AspNetCore/Cache/ClearCacheHandler.cs
@@ -18,34 +18,34 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-using System.Collections.Generic;
-using DbLocalizationProvider.Cache;
-using DbLocalizationProvider.Commands;
+//using System.Collections.Generic;
+//using DbLocalizationProvider.Cache;
+//using DbLocalizationProvider.Commands;
 
-namespace DbLocalizationProvider.AspNetCore.Cache
-{
-    public class ClearCacheHandler  : ICommandHandler<ClearCache.Command>
-    {
-        public void Execute(ClearCache.Command command)
-        {
-            if(HttpContext.Current == null)
-                return;
+//namespace DbLocalizationProvider.AspNetCore.Cache
+//{
+//    public class ClearCacheHandler  : ICommandHandler<ClearCache.Command>
+//    {
+//        public void Execute(ClearCache.Command command)
+//        {
+//            if(HttpContext.Current == null)
+//                return;
 
-            if(HttpContext.Current.Cache == null)
-                return;
+//            if(HttpContext.Current.Cache == null)
+//                return;
 
-            var itemsToRemove = new List<string>();
-            var enumerator = HttpContext.Current.Cache.GetEnumerator();
+//            var itemsToRemove = new List<string>();
+//            var enumerator = HttpContext.Current.Cache.GetEnumerator();
 
-            while(enumerator.MoveNext())
-            {
-                if(enumerator.Key.ToString().ToLower().StartsWith(CacheKeyHelper.CacheKeyPrefix.ToLower()))
-                    itemsToRemove.Add(enumerator.Key.ToString());
-            }
+//            while(enumerator.MoveNext())
+//            {
+//                if(enumerator.Key.ToString().ToLower().StartsWith(CacheKeyHelper.CacheKeyPrefix.ToLower()))
+//                    itemsToRemove.Add(enumerator.Key.ToString());
+//            }
 
-            foreach(var itemToRemove in itemsToRemove)
-                ConfigurationContext.Current.CacheManager.Remove(itemToRemove);
-        }
+//            foreach(var itemToRemove in itemsToRemove)
+//                ConfigurationContext.Current.CacheManager.Remove(itemToRemove);
+//        }
 
-    }
-}
+//    }
+//}
