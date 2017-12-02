7baa8125f163382ca37c1dd669bf7fa6b3429fbb
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2017-12-03 01:26:28 +0200

    fixed some merge issue

--- src/ClassLibrary1/Queries/GetTranslationHandler.cs
+++ src/ClassLibrary1/Queries/GetTranslationHandler.cs
@@ -19,9 +19,9 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
-using System.Data.Entity;
 using DbLocalizationProvider.Cache;
 using DbLocalizationProvider.Queries;
 
@@ -30,57 +30,58 @@ namespace DbLocalizationProvider.AspNet.Queries
     public class GetTranslationHandler : IQueryHandler<GetTranslation.Query, string>
     {
         public string Execute(GetTranslation.Query query)
+        {
+            if(!ConfigurationContext.Current.EnableLocalization())
+                return query.Key;
+
+            return GetTranslation(query);
+        }
+
+        protected virtual string GetTranslation(GetTranslation.Query query)
+        {
+            var key = query.Key;
+            var language = query.Language;
+            var cacheKey = CacheKeyHelper.BuildKey(key);
+            var localizationResource = ConfigurationContext.Current.CacheManager.Get(cacheKey) as LocalizationResource;
+
+            if(localizationResource != null)
+                return GetTranslationFromAvailableList(localizationResource.Translations, language, query.UseFallback)
+                    ?.Value;
+
+            var resource = GetResourceFromDb(key);
+            LocalizationResourceTranslation localization = null;
+
+            // create empty null resource - to indicate non-existing one
+            if(resource == null)
+                resource = LocalizationResource.CreateNonExisting(key);
+            else
+                localization = GetTranslationFromAvailableList(resource.Translations, language, query.UseFallback);
+
+            ConfigurationContext.Current.CacheManager.Insert(cacheKey, resource);
+            return localization?.Value;
+        }
+
+        protected virtual LocalizationResourceTranslation GetTranslationFromAvailableList(
+            ICollection<LocalizationResourceTranslation> translations,
+            CultureInfo language,
+            bool queryUseFallback)
+        {
+            var foundTranslation = translations?.FindByLanguage(language);
+            if(foundTranslation == null && queryUseFallback)
+                return translations?.FindByLanguage(CultureInfo.InvariantCulture);
+
+            return foundTranslation;
+        }
+
+        protected virtual LocalizationResource GetResourceFromDb(string key)
+        {
+            using(var db = new LanguageEntities())
             {
-                if(!ConfigurationContext.Current.EnableLocalization())
-                    return query.Key;
+                var resource = db.LocalizationResources
+                    .Include(r => r.Translations)
+                    .FirstOrDefault(r => r.ResourceKey == key);
 
-                return GetTranslation(query);
-            }
-
-            protected virtual string GetTranslation(GetTranslation.Query query)
-            {
-                var key = query.Key;
-                var language = query.Language;
-                var cacheKey = CacheKeyHelper.BuildKey(key);
-                var localizationResource = ConfigurationContext.Current.CacheManager.Get(cacheKey) as LocalizationResource;
-
-                if(localizationResource != null)
-                    return GetTranslationFromAvailableList(localizationResource.Translations, language, query.UseFallback)?.Value;
-
-                var resource = GetResourceFromDb(key);
-                LocalizationResourceTranslation localization = null;
-
-                // create empty null resource - to indicate non-existing one
-                if(resource == null)
-                    resource = LocalizationResource.CreateNonExisting(key);
-                else
-                    localization = GetTranslationFromAvailableList(resource.Translations, language, query.UseFallback);
-
-                ConfigurationContext.Current.CacheManager.Insert(cacheKey, resource);
-                return localization?.Value;
-            }
-
-            protected virtual LocalizationResourceTranslation GetTranslationFromAvailableList(ICollection<LocalizationResourceTranslation> translations,
-                                                                                             CultureInfo language,
-                                                                                             bool queryUseFallback)
-            {
-                var foundTranslation = translations?.FindByLanguage(language);
-                if(foundTranslation == null && queryUseFallback)
-                    return translations?.FindByLanguage(CultureInfo.InvariantCulture);
-
-                return foundTranslation;
-            }
-
-            protected virtual LocalizationResource GetResourceFromDb(string key)
-            {
-                using(var db = new LanguageEntities())
-                {
-                    var resource = db.LocalizationResources
-                        .Include(r => r.Translations)
-                        .FirstOrDefault(r => r.ResourceKey == key);
-
-                    return resource;
-                }
+                return resource;
             }
         }
     }
