64403b400603fc35c679d0ae1f33af2719327ae9
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-01-20 00:50:08 +0200

    Related to #129

--- src/DbLocalizationProvider.EPiServer.JsResourceHandler/Constants.cs
+++ src/DbLocalizationProvider.EPiServer.JsResourceHandler/Constants.cs
@@ -3,6 +3,7 @@
     internal class Constants
     {
         internal const string PathBase = "jsl10n";
+        internal const string DeepMergeScriptName = "deep-merge.js";
         internal const string IgnoreRoute = PathBase + "/" + "{*path}";
     }
 }
--- src/DbLocalizationProvider.EPiServer.JsResourceHandler/HttpHandler.cs
+++ src/DbLocalizationProvider.EPiServer.JsResourceHandler/HttpHandler.cs
@@ -20,7 +20,15 @@ namespace DbLocalizationProvider.EPiServer.JsResourceHandler
                                    ? languageSelector.Language.Name
                                    : context.Request.QueryString["lang"];
 
+            context.Response.ContentType = "text/javascript";
+
             var filename = ExtractFileName(context);
+
+            if(filename == Constants.DeepMergeScriptName)
+            {
+                context.Response.Write("/* https://github.com/KyleAMathews/deepmerge */ var jsResourceHandler=function(){function r(r){return!(c=r,!c||'object'!=typeof c||(t=r,n=Object.prototype.toString.call(t),'[object RegExp]'===n||'[object Date]'===n||(o=t,o.$$typeof===e)));var t,n,o,c}var e='function'==typeof Symbol&&Symbol.for?Symbol.for('react.element'):60103;function t(e,t){var n;return(!t||!1!==t.clone)&&r(e)?o((n=e,Array.isArray(n)?[]:{}),e,t):e}function n(r,e,n){return r.concat(e).map(function(r){return t(r,n)})}function o(e,c,a){var u,f,y,i,b=Array.isArray(c);return b===Array.isArray(e)?b?((a||{arrayMerge:n}).arrayMerge||n)(e,c,a):(f=c,y=a,i={},r(u=e)&&Object.keys(u).forEach(function(r){i[r]=t(u[r],y)}),Object.keys(f).forEach(function(e){r(f[e])&&u[e]?i[e]=o(u[e],f[e],y):i[e]=t(f[e],y)}),i):t(c,a)}return{deepmerge:function(r,e,t){return o(r,e,t)}}}();");
+                return;
+            }
 
             var debugMode = context.Request.QueryString["debug"] != null;
             var alias = string.IsNullOrEmpty(context.Request.QueryString["alias"]) ? "jsl10n" : context.Request.QueryString["alias"];
@@ -30,13 +38,12 @@ namespace DbLocalizationProvider.EPiServer.JsResourceHandler
             if(!(CacheManager.Get(cacheKey) is string responseObject))
             {
                 responseObject = _provider.Service.GetJson(filename, context, languageName, debugMode);
-                responseObject = $"window.{alias} = {responseObject}";
+                responseObject = $"window.{alias} = jsResourceHandler.deepmerge(window.{alias} || {{}}, {responseObject})";
 
                 CacheManager.Insert(cacheKey, responseObject);
             }
 
             context.Response.Write(responseObject);
-            context.Response.ContentType = "text/javascript";
         }
 
         public bool IsReusable { get; }
