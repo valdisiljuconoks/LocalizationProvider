b01b8e1ef14275957680f601a716c1e99b9fc61c
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-01-13 01:49:10 +0200

    * added .snupkg support * new version publish

--- build-packages.ps1
+++ build-packages.ps1
@@ -3,12 +3,16 @@ cd .\.nuget
 # common libraries
 cd .\..\src\DbLocalizationProvider.Abstractions\
 dotnet pack -c Release
+dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
 copy .\bin\Release\*.nupkg .\..\..\.nuget\
+copy .\bin\Release\*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
 
 cd .\..\src\DbLocalizationProvider\
 dotnet pack -c Release
+dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
 copy .\bin\Release\*.nupkg .\..\..\.nuget\
+copy .\bin\Release\*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
 
 cd ..\
