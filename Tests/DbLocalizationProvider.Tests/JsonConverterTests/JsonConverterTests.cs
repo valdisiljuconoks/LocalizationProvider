a0e9f96fcd9f52443a28bfbe7ec0b6dc0852aca4
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-01-13 15:29:13 +0200

    moved JsonConverter unit tests from aspnet lib to core

---
+++ Tests/DbLocalizationProvider.Tests/JsonConverterTests/JsonConverterTests.cs
@@ -0,0 +1,209 @@
+using System.Collections.Generic;
+using DbLocalizationProvider.Json;
+using Xunit;
+
+namespace DbLocalizationProvider.Tests.JsonConverterTests
+{
+    public class JsonConverterTests
+    {
+        [Fact]
+        public void VariousResources_WithMixedKeyNames()
+        {
+            var resources = new List<LocalizationResource>
+            {
+                new LocalizationResource("/another/mixmatch/key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is another english"
+                        }
+                    }
+                },
+                new LocalizationResource("This.Is.Resource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is english"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                }
+            };
+
+            var sut = new JsonConverter();
+
+            var resourcesAsJson = sut.Convert(resources, "en", true, false);
+
+            Assert.Equal("this is english", resourcesAsJson["This"]["Is"]["Resource"]["Key"]);
+
+            Assert.Equal(1, resourcesAsJson.Count);
+        }
+
+        [Fact]
+        public void VariousResourcesWithNorskTranslation_RequestedEnglishWothoutFallback_NoResults()
+        {
+            var resources = new List<LocalizationResource>
+            {
+                new LocalizationResource("This.Is.Resource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                },
+                new LocalizationResource("This.Is.AnotherResource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                }
+            };
+            var sut = new Json.JsonConverter();
+
+            var resourcesAsJson = sut.Convert(resources, "en", false, false);
+
+            Assert.Equal(0, resourcesAsJson.Count);
+        }
+
+        [Fact]
+        public void VariousResources_WithSharedRootKeyName()
+        {
+            var sut = new Json.JsonConverter();
+
+            var resources = new List<LocalizationResource>
+            {
+                new LocalizationResource("This.Is.Resource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is english"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                },
+                new LocalizationResource("This.Is.Resource.AnotherKey")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is another english"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is another norsk"
+                        }
+                    }
+                },
+                new LocalizationResource("This.Is.YetAnotherResource.AnotherKey")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is another english 2"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is another norsk 2"
+                        }
+                    }
+                }
+            };
+
+            var resourcesAsJson = sut.Convert(resources, "en", true, false);
+
+            Assert.Equal("this is english", resourcesAsJson["This"]["Is"]["Resource"]["Key"]);
+        }
+
+        [Fact]
+        public void ResoureceWithMultipleTranslations_ReturnRequestedTranslation()
+        {
+            var sut = new Json.JsonConverter();
+
+            var resources = new List<LocalizationResource>
+            {
+                new LocalizationResource("This.Is.Resource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is english"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                }
+            };
+
+            var resourcesAsJson = sut.Convert(resources, "no", true, false);
+
+            Assert.Equal("this is norsk", resourcesAsJson["This"]["Is"]["Resource"]["Key"]);
+        }
+
+        [Fact]
+        public void Resourece_SerializeWithCamelCase()
+        {
+            var sut = new Json.JsonConverter();
+
+            var resources = new List<LocalizationResource>
+            {
+                new LocalizationResource("This.Is.TheResource.Key")
+                {
+                    Translations = new List<LocalizationResourceTranslation>
+                    {
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "en",
+                            Value = "this is english"
+                        },
+                        new LocalizationResourceTranslation
+                        {
+                            Language = "no",
+                            Value = "this is norsk"
+                        }
+                    }
+                }
+            };
+
+            var resourcesAsJson = sut.Convert(resources, "en", true, true);
+
+            Assert.Equal("this is english", resourcesAsJson["this"]["is"]["theResource"]["key"]);
+        }
+    }
+}
