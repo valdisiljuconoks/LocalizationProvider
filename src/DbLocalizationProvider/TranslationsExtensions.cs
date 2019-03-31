e02867a96351984b0b0f5e1ee1035a3b031afc58
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-03-31 21:46:50 +0300

    resource key encoding in xliff files

--- src/DbLocalizationProvider/TranslationsExtensions.cs
+++ src/DbLocalizationProvider/TranslationsExtensions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Valdis Iljuconoks.
+// Copyright (c) 2019 Valdis Iljuconoks.
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without
@@ -27,11 +27,6 @@ namespace DbLocalizationProvider
 {
     public static class TranslationsExtensions
     {
-        public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, CultureInfo language)
-        {
-            return ByLanguage(translations, language.Name);
-        }
-
         public static LocalizationResourceTranslation FindByLanguage(this ICollection<LocalizationResourceTranslation> translations, CultureInfo language)
         {
             return FindByLanguage(translations, language.Name);
@@ -40,6 +35,22 @@ namespace DbLocalizationProvider
         public static LocalizationResourceTranslation FindByLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
         {
             return translations.FirstOrDefault(t => t.Language == language);
+        }
+
+        public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, CultureInfo language)
+        {
+            return ByLanguage(translations, language.Name);
+        }
+
+        // TODO: add this to the major version
+        //public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, CultureInfo language, bool invariantCultureFallback)
+        //{
+
+        //}
+
+        public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
+        {
+            return ByLanguage(translations, language, ConfigurationContext.Current.EnableInvariantCultureFallback);
         }
 
         public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, string language, bool invariantCultureFallback)
@@ -54,11 +65,6 @@ namespace DbLocalizationProvider
             return translation != null ?
                        translation.Value :
                        invariantCultureFallback ? translations.FindByLanguage(string.Empty)?.Value : string.Empty;
-        }
-
-        public static string ByLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
-        {
-            return ByLanguage(translations, language, ConfigurationContext.Current.EnableInvariantCultureFallback);
         }
 
         public static bool ExistsLanguage(this ICollection<LocalizationResourceTranslation> translations, string language)
