0ef272cb1e7f80fa591f1e6b4a66f91ab61f7433
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-04 08:26:39 +0200

    Added sync command https://github.com/valdisiljuconoks/localization-provider-core/issues/22

---
+++ src/DbLocalizationProvider/Sync/SyncResources.cs
@@ -0,0 +1,19 @@
+using DbLocalizationProvider.Abstractions;
+
+namespace DbLocalizationProvider.Sync
+{
+    /// <summary>
+    ///     Implement handler for this command when you are asked to synchronize resources from code to storage implementation
+    ///     of your choice
+    /// </summary>
+    public class SyncResources
+    {
+        /// <summary>
+        ///     Implement handler for this command when you are asked to synchronize resources from code to storage implementation
+        ///     of your choice
+        /// </summary>
+        public class Command : ICommand
+        {
+        }
+    }
+}
