c29ad733e0eae9263827d1c87e229ce94b714cbd
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-05 07:59:46 +0200

    Added new command to delete all resources
    https://github.com/valdisiljuconoks/localization-provider-core/issues/22

---
+++ src/DbLocalizationProvider/Commands/DeleteAllResources.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Valdis Iljuconoks. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using DbLocalizationProvider.Abstractions;
+
+namespace DbLocalizationProvider.Commands
+{
+    public class DeleteAllResources
+    {
+        public class Command : ICommand { }
+    }
+}
