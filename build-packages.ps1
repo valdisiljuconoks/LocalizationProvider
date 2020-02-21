19b2d79033c2b943a5acc44eac01ed02b8167e93
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-02-21 08:27:01 +0200

    fixed mssql package name

--- build-packages.ps1
+++ build-packages.ps1
@@ -15,4 +15,11 @@ copy .\bin\Release\*.nupkg .\..\..\.nuget\
 copy .\bin\Release\*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
 
+cd .\..\src\DbLocalizationProvider.Storage.SqlServer\
+dotnet pack -c Release
+dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
+copy .\bin\Release\*.nupkg .\..\..\.nuget\
+copy .\bin\Release\*.snupkg .\..\..\.nuget\
+cd .\..\..\.nuget\
+
 cd ..\
