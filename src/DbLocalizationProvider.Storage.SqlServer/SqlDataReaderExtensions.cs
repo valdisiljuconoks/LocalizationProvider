08b9814d7a53632dffe9c2291ed9e22f63a08ab4
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-03-15 11:45:46 +0200

    Added file header for the Apache-2.0 license

--- src/DbLocalizationProvider.Storage.SqlServer/SqlDataReaderExtensions.cs
+++ src/DbLocalizationProvider.Storage.SqlServer/SqlDataReaderExtensions.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Valdis Iljuconoks. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
 using Microsoft.Data.SqlClient;
 
 namespace DbLocalizationProvider.Storage.SqlServer
