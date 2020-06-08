f395f4135d30dc5d9c79dde21f253b015341dcab
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2020-06-08 22:57:36 +0300

    added postresql to build script

--- build-packages.ps1
+++ build-packages.ps1
@@ -15,7 +15,15 @@ copy .\bin\Release\*.nupkg .\..\..\.nuget\
 copy .\bin\Release\*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
 
+# storage
 cd .\..\src\DbLocalizationProvider.Storage.SqlServer\
+dotnet pack -c Release
+dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
+copy .\bin\Release\*.nupkg .\..\..\.nuget\
+copy .\bin\Release\*.snupkg .\..\..\.nuget\
+cd .\..\..\.nuget\
+
+cd .\..\src\DbLocalizationProvider.Storage.PostreSql\
 dotnet pack -c Release
 dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
 copy .\bin\Release\*.nupkg .\..\..\.nuget\
