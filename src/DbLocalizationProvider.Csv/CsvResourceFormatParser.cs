ef106fa6d7b9c88f4393b84f4f9f624bbbd3f62b
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2021-01-11 07:43:04 +0200

    Fixed copyright info

--- src/DbLocalizationProvider.Csv/CsvResourceFormatParser.cs
+++ src/DbLocalizationProvider.Csv/CsvResourceFormatParser.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Valdis Iljuconoks. All rights reserved.
+// Copyright (c) Mattias Olsson, Valdis Iljuconoks. All rights reserved.
 // Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
 using System;
