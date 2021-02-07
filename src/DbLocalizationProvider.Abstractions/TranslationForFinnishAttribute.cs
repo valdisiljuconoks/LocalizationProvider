f43973a60b00a3b5508acd19c616b102b434aae0
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2021-02-07 10:27:57 +0200

    Closes #205

---
+++ src/DbLocalizationProvider.Abstractions/TranslationForFinnishAttribute.cs
@@ -0,0 +1,14 @@
+namespace DbLocalizationProvider.Abstractions
+{
+    /// <summary>
+    /// If you wanna provide additional translations Norwegian (`fi`) - use this attribute.
+    /// </summary>
+    public class TranslationForFinnishAttribute : TranslationForCultureAttribute
+    {
+        /// <summary>
+        /// Initiates new instance of attribute with `fi` culture.
+        /// </summary>
+        /// <param name="translation"></param>
+        public TranslationForFinnishAttribute(string translation) : base(translation, "fi") { }
+    }
+}
