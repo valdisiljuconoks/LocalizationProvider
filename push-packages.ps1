07df45f1421b8eb794b473116ac8534384f0e732
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2022-03-02 22:51:36 +0200

    set version to 7.2

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,12 +1,12 @@
 cd .\.nuget
 
-.\nuget.exe push LocalizationProvider.Abstractions.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.Models.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.Csv.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.Storage.AzureTables.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.Storage.PostgreSql.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.Storage.SqlServer.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.Xliff.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Abstractions.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.AdminUI.Models.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Csv.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Storage.AzureTables.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Storage.PostgreSql.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Storage.SqlServer.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Xliff.7.2.0.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
