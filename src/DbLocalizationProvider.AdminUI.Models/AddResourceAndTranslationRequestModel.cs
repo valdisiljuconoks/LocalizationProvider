b1c1fb7c7fcbbfa898a47ba9a1e75e2482ff983e
Surjit Bharath <surjit.bharath@hiddenfoundry.com>
2022-08-18 23:15:36 +0100

    Request model for adding a resource and translation for use by ServiceController in the DBLocalizationProvider repository.

---
+++ src/DbLocalizationProvider.AdminUI.Models/AddResourceAndTranslationRequestModel.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Valdis Iljuconoks. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using Newtonsoft.Json;
+
+namespace DbLocalizationProvider.AdminUI.Models
+{
+    /// <summary>
+    /// Create or update resource translation
+    /// </summary>
+    [JsonObject]
+    public class AddResourceAndTranslationRequestModel
+    {
+        /// <summary>
+        /// Resource key
+        /// </summary>
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// For which language
+        /// </summary>
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        /// <summary>
+        /// What is new translation
+        /// </summary>
+        [JsonProperty("translation")]
+        public string Translation { get; set; }
+    }
+}
