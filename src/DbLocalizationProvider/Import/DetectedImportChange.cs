32996f3f0e57479af434da58e27e88b305b7ee82
valdis <valdis.iljuconoks@outlook.com>
2022-11-06 18:31:35 +0200

    Changed Culture model for import changes

--- src/DbLocalizationProvider/Import/DetectedImportChange.cs
+++ src/DbLocalizationProvider/Import/DetectedImportChange.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Valdis Iljuconoks. All rights reserved.
 // Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
+using System;
 using System.Collections.Generic;
 using DbLocalizationProvider.Abstractions;
 
@@ -27,7 +28,7 @@ namespace DbLocalizationProvider.Import
             ChangeType = changeType;
             ImportingResource = importing;
             ExistingResource = existing;
-            ChangedLanguages = new List<string>();
+            ChangedLanguages = new List<CultureModel>();
         }
 
         /// <summary>
@@ -56,6 +57,39 @@ namespace DbLocalizationProvider.Import
         /// <summary>
         /// Gets or sets list of changed languages.
         /// </summary>
-        public ICollection<string> ChangedLanguages { get; set; }
+        public ICollection<CultureModel> ChangedLanguages { get; set; }
+
+        /// <summary>
+        /// Description class for supported languages
+        /// </summary>
+        public class CultureModel
+        {
+            /// <summary>
+            /// Creates new instance
+            /// </summary>
+            /// <param name="code">ISO code of the language (e.g. en-US)</param>
+            /// <param name="display">Display name of the language</param>
+            public CultureModel(string code, string display)
+            {
+                Code = code ?? throw new ArgumentNullException(nameof(code));
+                Display = display ?? throw new ArgumentNullException(nameof(display));
+                TitleDisplay = $"{display}{(code != string.Empty ? " (" + code + ")" : string.Empty)}";
+            }
+
+            /// <summary>
+            /// ISO code of the language (e.g. en-US)
+            /// </summary>
+            public string Code { get; }
+
+            /// <summary>
+            /// Display name of the language
+            /// </summary>
+            public string Display { get; }
+
+            /// <summary>
+            /// Display name of the language in the title bar of the modal window
+            /// </summary>
+            public string TitleDisplay { get; }
+        }
     }
 }
