c893a52c79ca528dcc79a9dcc8cc84275e2abbc3
valdis <valdis.iljuconoks@outlook.com>
2023-01-02 00:04:20 +0200

    nuget update

--- Tests/DbLocalizationProvider.Tests/KnownAttributesTests/_Tests.cs
+++ Tests/DbLocalizationProvider.Tests/KnownAttributesTests/_Tests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using DbLocalizationProvider.Internal;
 using DbLocalizationProvider.Queries;
 using DbLocalizationProvider.Refactoring;
 using DbLocalizationProvider.Sync;
