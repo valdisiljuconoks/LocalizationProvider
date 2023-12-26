8184f34bfdaca7efe2773c43989724e70eab10a7
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2023-12-26 09:14:53 +0200

    added azure translator proejct to the build pipeline

--- push-packages.ps1
+++ push-packages.ps1
@@ -8,5 +8,6 @@ cd .\.nuget
 .\nuget.exe push LocalizationProvider.Storage.PostgreSql.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
 .\nuget.exe push LocalizationProvider.Storage.SqlServer.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
 .\nuget.exe push LocalizationProvider.Xliff.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.Translator.Azure.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
