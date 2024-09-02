c75eef4a277fb8fea8febf1a33f6e0abd3e828e0
Valdis Iljuconoks <valdis@getadigital.com>
2024-09-02 06:09:35 +0300

    set v8.1.6

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,13 +1,13 @@
 cd .\.nuget
 
-nuget push LocalizationProvider.Abstractions.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.Models.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Csv.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.AzureTables.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.PostgreSql.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.SqlServer.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Xliff.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Translator.Azure.8.1.5.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Abstractions.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.Models.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Csv.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.AzureTables.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.PostgreSql.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.SqlServer.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Xliff.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Translator.Azure.8.1.6.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
