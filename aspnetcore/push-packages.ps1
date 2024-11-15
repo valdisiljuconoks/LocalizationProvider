0dfe67d37153bda01c29c4bf8462e34220caa096
Valdis Iljuconoks <valdis@getadigital.com>
2024-11-15 15:24:39 +0200

    set v8.2.2

--- aspnetcore/push-packages.ps1
+++ aspnetcore/push-packages.ps1
@@ -3,9 +3,9 @@
 
 cd .\.nuget
 
-nuget push LocalizationProvider.AspNetCore.8.2.0.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.2.0.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.2.0.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.8.2.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AspNetCore.8.2.2.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.2.2.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.2.2.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.8.2.2.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
