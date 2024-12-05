d9f72b0aaa6e9f736cb7f8ba45efe7b7536e09b7
valdis iljuconoks <valdis.iljuconoks@outlook.com>
2024-12-05 21:29:55 +0200

    target net9

--- common/push-packages.ps1
+++ common/push-packages.ps1
@@ -1,13 +1,13 @@
 cd .\.nuget
 
-nuget push LocalizationProvider.Abstractions.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.Models.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Csv.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.AzureTables.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.PostgreSql.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Storage.SqlServer.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Xliff.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.Translator.Azure.8.2.3.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Abstractions.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.Models.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Csv.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.AzureTables.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.PostgreSql.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Storage.SqlServer.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Xliff.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.Translator.Azure.9.0.0.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
