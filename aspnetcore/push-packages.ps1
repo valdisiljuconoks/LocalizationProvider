f92959af68f0e7030e189a1e3d5820f73d7ec570
valdis <valdis@getadigital.com>
2025-07-04 09:49:49 +0300

    updated script with correct versions

--- aspnetcore/push-packages.ps1
+++ aspnetcore/push-packages.ps1
@@ -3,9 +3,9 @@
 
 cd .\.nuget
 
-nuget push LocalizationProvider.AspNetCore.8.2.4.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.2.4.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.2.4.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.8.2.4.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AspNetCore.8.2.5.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.2.5.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.2.5.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.8.2.5.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
